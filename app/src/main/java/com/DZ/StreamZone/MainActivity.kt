b6fb9ce74fb1b09409c4e8f7765ec66c851ab8e3
LotfiDz13 <lotfichabounia@gmail.com>
2026-08-01 16:08:47 +0100

    First commit

---
+++ app/src/main/java/com/DZ/StreamZone/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.DZ.StreamZone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
